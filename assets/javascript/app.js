d185360c0c00fcdc0f61b7c90823791437172cbc
Guillermo Villalta <mexcelus@gmail.com>
2019-08-10 15:32:26 -0600

    added data attribute for button with the element name + alert the attribute with click event listener

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,12 @@ for (var i = 0; i < fruits.length; i++) {
     console.log(i);
     var button = $("<button></button>");
     button.text(fruits[i]);
+    button.attr('data', fruits[i]);
     button.addClass("run-api")
     $("#buttons-here").append(button);
     
 }
+
+$(".run-api").click( function() {
+    alert($(this).attr('data'))
+})
